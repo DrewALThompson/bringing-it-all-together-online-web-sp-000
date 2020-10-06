b35fd4909e56e4cc347e23f7416f0a33d0a2da10
Drew Thompson <drewallynthompson@gmail.com>
2020-10-06 02:33:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -66,7 +66,7 @@ class Dog
     dog
   end
     
-  def self.find_by_name(name)
+  def self.find_by_name(name: )
     sql = "SELECT * FROM dogs WHERE name = ?"
     DB[:conn].execute(sql, name).map{|row|self.new_from_db(row)}[0]
   end
