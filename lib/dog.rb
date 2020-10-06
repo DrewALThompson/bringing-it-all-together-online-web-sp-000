c779442349633fea05739b3a5e20e631731690c0
Drew Thompson <drewallynthompson@gmail.com>
2020-10-06 02:22:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -55,7 +55,7 @@ class Dog
     DB[:conn].execute(sql, id).map{|row| self.new_from_db(row)}[0]
   end
   
-  def self.find_or_create_by
+  def self.find_or_create_by(name: name, breed: breed)
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
     if !dog.empty?
       
