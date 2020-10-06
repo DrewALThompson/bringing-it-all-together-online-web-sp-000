d160bebfb8e557c99184023d8ee146c08a35de72
Drew Thompson <drewallynthompson@gmail.com>
2020-10-06 02:25:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -61,7 +61,7 @@ class Dog
       row = dog[0]
       dog = Dog.new(row[0], row[1], row[2])
     else
-      dog = Dog.new(name: name, breed: breed)
+      dog = Dog.create(name: name, breed: breed)
     end
     dog
   end
